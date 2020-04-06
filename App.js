6bad0de844d682776a8b6cf3d4aa7621de3607eb
David Castillo <dcastillo@Davids-MBP.fios-router.home>
2020-04-05 21:30:14 -0400

    I changed the background color from white to beige

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'beige',
     alignItems: 'center',
     justifyContent: 'center',
   },
