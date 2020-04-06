7f0a959fcbcd9c2b44edf8778b5d24c1ac909ee2
David Castillo <dcastillo@Davids-MBP.fios-router.home>
2020-04-05 21:48:27 -0400

    Button Has been Made

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity ,Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MENU</Text>
+      <TouchableOpacity 
+      onPress = {() => alert('Joke time')}
+      style = {{backgroundColor: ' '}}>
+      <Text style = {{fontSize: 20, color: 'black'}}>
+      Stil Bored? Click me
+      </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'beige',
     alignItems: 'center',
     justifyContent: 'center',
   },
