f608960979d091eb3de3a375edaabf766e34a275
David Castillo <dcastillo@Davids-MBP.fios-router.home>
2020-04-15 14:01:25 -0400

    new game engine installed

--- screens/Flappy_Bird.js
+++ screens/Flappy_Bird.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { StyleSheet,Text, View, Button,ImageBackground } from 'react-native';
-import { BorderlessButton } from 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { StyleSheet,Text, View,StatusBar,Alert, Button, ImageBackground, TouchableOpacity } from 'react-native';
+import Constants from '../components/Constants';
+//import { BorderlessButton } from 'react-native-gesture-handler';
 
-export default function Flappy(){
-    return (
-        <ImageBackground source= {require('../assets/Flappy.png')} style = {styles.container}>
-            <Text style = {styles.Item2}>Flappy Bird Game Coming Soon</Text>
-        </ImageBackground>
-    )
-}
+// export default function Flappy(){
+//     return (
+//         <ImageBackground source= {require('../assets/Flappy.png')} style = {styles.container}>
+//             <Text style = {styles.Item2}>Flappy Bird Game Coming Soon</Text>
+//         </ImageBackground>
+//     )
+// }
+
+
+export default class Flappy extends Component{
+    constructor (props){
+        super (props);
+    }
+
+    render (){
+        return (
+            <ImageBackground source= {require('../assets/Flappy.png')} style = {styles.container}>
+                <Text style = {styles.Item2}>Flappy Bird Game Coming Soon</Text>
+            </ImageBackground>
+        )
+    }
+};
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 40,
-        backgroundColor: 'beige',
-        //ImageBackground:`url(https://www.desktopbackground.org/download/2304x864/2014/07/26/799254_flappy-generator-plus-create-your-own-flappy-bird-game_9600x950_h.png)` 
     },
     Item2:{
         textAlign: 'center',
         fontSize: 20,
     }
 });
+
+
