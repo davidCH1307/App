f342cd12e65ef5259497478d1600743823bd15e7
David Castillo <dcastillo@Davids-MBP.fios-router.home>
2020-04-15 21:33:40 -0400

    flappy bird is almost functioning

--- components/Physics.js
+++ components/Physics.js
@@ -7,6 +7,12 @@ const Physics = (entities, {touches, time}) => {
     touches.filter(t => t.type === 'press' ).forEach(t => {
         Matter.Body.applyForce(bird, bird.position, { x: 0.0, y: -0.1});
     });
+
+    for (let i = 1; i <= 4; i++){
+        Matter.Body.translate(entities["pipe" + i].body, {x: -1, y: 0 })
+    }
+
+
     Matter.Engine.update(engine, time.delta);
 
 
--- screens/Flappy_Bird.js
+++ screens/Flappy_Bird.js
@@ -8,6 +8,24 @@ import Wall from '../components/Wall';
 import Physics from '../components/Physics';
 
 
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+export const generatePipes = () => {
+    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT/2) -100);
+    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+
+    let sizes = [topPipeHeight, bottomPipeHeight];
+
+    if (Math.random() <0.5){
+        sizes = sizes.reverse();
+    }
+
+    return sizes; 
+
+}
+
 export default class Flappy extends Component{
     constructor (props){
         super (props);
@@ -23,18 +41,26 @@ export default class Flappy extends Component{
         let floor = Matter.Bodies.rectangle(Constants.MAX_WIDTH /2, Constants.MAX_HEIGHT -110, Constants.MAX_WIDTH, 50, { isStatic: true });
         let ceiling = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true});
         
+        let [pipe1Height, pipe2Height] = generatePipes();
+        let pipe1 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH /2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+        let pipe2 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH /2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
         
+        let [pipe3Height, pipe4Height] = generatePipes();
+        let pipe3 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH /2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
+        let pipe4 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH /2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
         
-        
-        
-        
-        Matter.World.add(world, [bird, floor]);
+
+        Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
 
         return {
             physics: {engine: engine, world: world },
             bird: {body: bird, size: [50, 50], color: 'brown', renderer: Bird },
             floor: {body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
             ceiling: {body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
+            pipe1: {body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: 'green', renderer: Wall },
+            pipe2: {body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: 'green', renderer: Wall },
+            pipe3: {body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: 'green', renderer: Wall },
+            pipe4: {body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: 'green', renderer: Wall },
         }
     } 
 
